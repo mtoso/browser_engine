94b118fd083ab2b1288360b17c97dbaaba6ceceb
Mattia Toso <mattia@mattias-macbook.gateway.sonic.net>
2019-06-30 21:28:23 -0700

    Add more DOM code

--- src/dom.rs
+++ src/dom.rs
@@ -1,32 +1,52 @@
-use::std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fmt;
 
-struct Node {
-    children: Vec<Node>, 
+pub struct Node {
+    children: Vec<Node>,
     node_type: NodeType,
 }
-
-enum NodeType {
-    Text(String),
-    Element(ElementData),
-    Comment(String)
-}
-
-struct ElementData {
-    tag_name: String,
-    attributes: AttrMap
-}
-
-type AttrMap = HashMap<String, String>;
 
 impl Node {
     fn new(node_type: NodeType, children: Vec<Node>) -> Node {
         Node {
             node_type,
-            children
+            children,
         }
     }
 }
+
+pub enum NodeType {
+    Text(String),
+    Element(ElementData),
+    Comment(String),
+}
+
+pub struct ElementData {
+    tag_name: String,
+    attributes: AttrMap,
+}
+
+impl ElementData {
+    fn new(tag_name: String, attributes: AttrMap) -> ElementData {
+        ElementData {
+            tag_name,
+            attributes,
+        }
+    }
+
+    fn get_id(&self) -> Option<&String> {
+        self.attributes.get("id")
+    }
+
+    fn get_classes(&self) -> HashSet<&str> {
+        match self.attributes.get("class") {
+            Some(s) => s.split(' ').collect(),
+            None => HashSet::new(),
+        }
+    }
+}
+
+pub type AttrMap = HashMap<String, String>;
 
 impl fmt::Debug for Node {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -51,5 +71,22 @@ impl fmt::Debug for ElementData {
             attributes_string.push_str(&format!(" {}=\"{}\"", attr, value));
         }
         write!(f, "<{},{}>", self.tag_name, attributes_string)
+    }
+}
+
+pub fn pretty_print(n: &Node, indent_size: usize) {
+    let indent = (0..indent_size).map(|_| " ").collect::<String>();
+
+    match n.node_type {
+        NodeType::Element(ref e) => println!("{}{:?}", indent, e),
+        NodeType::Text(ref t) => println!("{}{}", indent, t),
+        NodeType::Comment(ref c) => println!("{}<!--{}-->", indent, c),
+    }
+    for child in n.children.iter() {
+        pretty_print(&child, indent_size + 2);
+    }
+    match n.node_type {
+        NodeType::Element(ref e) => println!("{}<{}/>", indent, e.tag_name),
+        _ => {}
     }
 }
